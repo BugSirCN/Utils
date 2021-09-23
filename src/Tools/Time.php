de379800edd705c3827d797cd33494df675d167b
Train(MBP16) <zeng@imkd.net>
2021-09-23 18:16:56 +0800

    时间戳工具

--- src/Tools/Time.php
+++ src/Tools/Time.php
@@ -2,11 +2,25 @@
 
 namespace Bugsir\Utils\Tools;
 
+use Exception;
+
 /**
  * 时间工具
  */
 class Time
 {
+    const ENUM_TIMESTAMP_TYPE_LAST_MONTH = 'LAST_MONTH';//上月时间戳
+    const ENUM_TIMESTAMP_TYPE_THIS_MONTH = 'THIS_MONTH';//本月时间戳
+    const ENUM_TIMESTAMP_TYPE_NEXT_MONTH = 'NEXT_MONTH';//下月时间戳
+
+    const ENUM_TIMESTAMP_TYPE_LAST_YEAR = 'LAST_YEAR';//去年时间戳
+    const ENUM_TIMESTAMP_TYPE_THIS_YEAR = 'THIS_YEAR';//今年时间戳
+    const ENUM_TIMESTAMP_TYPE_NEXT_YEAR = 'NEXT_YEAR';//明年时间戳
+
+    const ENUM_TIMESTAMP_TYPE_LAST_WEEK = 'LAST_WEEK';//上周时间戳
+    const ENUM_TIMESTAMP_TYPE_THIS_WEEK = 'THIS_WEEK';//本周时间戳
+    const ENUM_TIMESTAMP_TYPE_NEXT_WEEK = 'NEXT_WEEK';//下周时间戳
+
     /**
      * 近期时间改为"人话"，比如X秒前、X天后
      * @param int $unixTime 需要转义的时间
@@ -54,4 +68,49 @@ class Time
         }
         return date($tempDay . ' ' . $tempTime, $unixTime);
     }
+
+    /**
+     * 获取特定时间戳
+     * @throws Exception
+     */
+    public function getTimestamp($type, $nowTime = null)
+    {
+        if (is_null($nowTime)) {
+            $nowTime = time();
+        }
+
+        switch ($type) {
+            case static::ENUM_TIMESTAMP_TYPE_LAST_MONTH://上月
+                return mktime(0, 0, 0, date('m', $nowTime) - 1, 1, date('Y', $nowTime));
+
+            case static::ENUM_TIMESTAMP_TYPE_THIS_MONTH://本月
+                return strtotime(date('Y-m', $nowTime));
+
+            case static::ENUM_TIMESTAMP_TYPE_NEXT_MONTH://下月
+                return mktime(0, 0, 0, date('m', $nowTime) + 1, 1, date('Y', $nowTime));
+
+            case static::ENUM_TIMESTAMP_TYPE_LAST_YEAR://去年
+                return mktime(0, 0, 0, 1, 1, date('Y', $nowTime) - 1);
+
+            case static::ENUM_TIMESTAMP_TYPE_THIS_YEAR://今年
+                return mktime(0, 0, 0, 1, 1, date('Y', $nowTime));
+
+            case static::ENUM_TIMESTAMP_TYPE_NEXT_YEAR://明年
+                return mktime(0, 0, 0, 1, 1, date('Y', $nowTime) + 1);
+
+            case static::ENUM_TIMESTAMP_TYPE_LAST_WEEK://上周（周一是第一天）
+                return strtotime('today', $nowTime) - 86400 * (date('N', $nowTime) + 6);
+
+            case static::ENUM_TIMESTAMP_TYPE_THIS_WEEK://本周（周一是第一天）
+                return strtotime('today', $nowTime) - 86400 * (date('N', $nowTime) - 1);
+
+            case static::ENUM_TIMESTAMP_TYPE_NEXT_WEEK://下周（周一是第一天）
+                return strtotime('today', $nowTime) - 86400 * (date('N', $nowTime) - 8);
+            default:
+                throw new Exception('未定义的时间类型');
+        }
+
+    }
+
+
 }
